7b7334536f3ce03316dd8212be6e821088a09816
BlackDragon <73701060+BlackDragon0207@users.noreply.github.com>
2021-06-11 23:37:00 +0900

    Update cmd.js

--- commands/cmd.js
+++ commands/cmd.js
@@ -9,11 +9,28 @@ module.exports = {
 async run (client, message, args) {
 
     if(!message.author.id == ['435800525389430804','616570697875193866','690504046905393182']) return message.reply('당신은 개발자가 아닙니다.')
-    if(!args) return message.channel.send('관리자님, Text를 적어주세요!')
-const Eval = new Discord.MessageEmbed()
-.setTitle("cmd 실행 완료")
-.setDescription('```md\n# '+args.join(' ')+'\n```\n\n'+'```js\n'+eval(args.join(' '))+'```')
-.setFooter(message.author.tag,message.author.displayAvatarURL())
-message.channel.send(Eval)
-  }
-}
+
+        const input = args.join(' ')
+        if(input.length < 1) return message.reply('Text를 적어주세요!')
+
+        // Actual Eval
+        try {
+            const result = eval(input)
+
+            const embed = new MessageEmbed()
+                .setTitle("실행 완료")
+                .setDescription(`⌨Input\`\`\`md\n${input}\n\`\`\`\n🖥Output\`\`\`js\n${result}\n\`\`\``)
+                
+                .setColor(this.client.config.color)
+                .setAuthor('Made by 놀욘#0132','https://cdn.discordapp.com/attachments/820186973624074240/852820765887299594/086dbed89b457be2.png')
+            message.channel.send(embed)
+        } catch (e) {
+            console.error(e)
+            const embed = new MessageEmbed()
+                .setTitle('에러')
+                .setDescription(`⌨Input\`\`\`md\n${input}\n\`\`\`\n🖥Output\`\`\`js\n${e.message}\n\`\`\``)
+                .setColor('RED')
+                .setFooter("Made by. 놀욘#0132", client.user.displayAvatarURL())
+               
+            return message.channel.send(embed)
+        }
