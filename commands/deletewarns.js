433487c37fed70aa09c10b789a94155649287d79
BlackDragon <73701060+BlackDragon0207@users.noreply.github.com>
2021-06-16 20:22:16 +0900

    Update deletewarns.js

--- commands/deletewarns.js
+++ commands/deletewarns.js
@@ -20,7 +20,7 @@ module.exports = {
         if(user.id === message.author.id) return message.channel.send('You can\'t clear your own warnings');
 
 
-        db.delete(`warnings_${message.guild.id}_${user.id}`);
+            db.add(`warnings_${message.guild.id}_${user.id}`, -1)
 
         setTimeout (() => {
             const warnEmbed = new Discord.MessageEmbed()
